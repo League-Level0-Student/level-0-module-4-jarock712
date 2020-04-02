8987d81bf34cb68ee85fd5e6116268b4fd0c4888
jaxon <jaxon@WendyTribe2>
2020-04-02 16:33:38 -0700

    good day

--- src/_01_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_01_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,8 +1,14 @@
-
 void setup() {
-  
+  size(500, 500);
+PImage face = loadImage("download.jpg");
+  face.resize(500, 500);
+  background(face);
 }
 
 void draw() {
-  
+ellipse(300, 190, 40, 70);
+}
+void pupil(){
+  fill(0,0,0);
+  ellipse(300, 190, 20, 40);
 }
--- src/_03_change_calculator/ChangeCalculator.java
+++ src/_03_change_calculator/ChangeCalculator.java
@@ -1,31 +1,26 @@
 package _03_change_calculator;
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
-
-
 import javax.swing.JOptionPane;
-
 /*
  * I have a pocket full of change. I hate doing math. Make me a program that
  * will calculate how much money I have without me having to use my brain. Then
  * make me a sandwich.
  */
 public class ChangeCalculator {
-
 	public static void main(String[] args) {
-
 		// Ask the user how many nickels they have
-
+		String nickel = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int nickels = Integer.parseInt(nickel);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dime = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dimes = Integer.parseInt(dime);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quarter = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quarters = Integer.parseInt(quarter);
 		// Calculate how much money the user has and save it in a double variable 
-
+		double value = (nickels*0.05) + (dimes*0.1) + (quarters*0.25);
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, "$" + value);
 	}
 }
 
